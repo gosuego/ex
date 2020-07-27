495bb0e80bc4e3f5f4696f95c6e312737fc214b5
gosuego <cankate@hanmail.net>
2020-07-27 13:57:53 +0900

    '4교시'

--- DB 실습/7장(PLSQL).sql
+++ DB 실습/7장(PLSQL).sql
@@ -13,3 +13,15 @@ begin
     dbms_output.put_line('오라클입니다.');
 end;
 /
+--예시2
+declare
+    vempno number(4);
+    vename varchar2(10);
+begin
+    vempno := 1001;
+    vename :='김사랑';
+    dbms_output.put_line('사번 이름');
+    dbms_output.put_line('--------------------');
+    dbms_output.put_line(' '||vempno||' '||vename);
+end;
+/
