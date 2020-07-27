1eadbd381166e7b18e3711c153bd64dc96bd3786
gosuego <cankate@hanmail.net>
2020-07-27 13:40:53 +0900

    '7장'

---
+++ DB 실습/7장(PLSQL).sql
@@ -0,0 +1,15 @@
+--7장 PL/SQL
+--예시 1
+
+set serveroutput on;
+
+begin
+    dbms_output.put_line('Hello Word');
+end;
+/
+
+begin
+    dbms_output.put_line('안녕하세요');
+    dbms_output.put_line('오라클입니다.');
+end;
+/
