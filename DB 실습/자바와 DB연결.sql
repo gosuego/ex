fa55a7eec76c513270ae1b1f1311956904df8910
gosuego <cankate@hanmail.net>
2020-07-29 10:29:28 +0900

    '자바DB연결'

--- DB 실습/자바와 DB연결.sql
+++ DB 실습/자바와 DB연결.sql
@@ -15,3 +15,14 @@ price number
 );
 
 create sequence book_no_seq increment by 1 start with 1;
+
+insert into books (book_id,title,publisher,year,price)
+values(book_no_seq.nextval,'Operating System Concepts','Wiley','2003',30700);
+insert into books (book_id,title,publisher,year,price)
+values(book_no_seq.nextval,'Head First PHP and MYSQL','Oreilly','2009',58000);
+insert into books (book_id,title,publisher,year,price)
+values(book_no_seq.nextval,'C Programming Language','Prentice-Hall','1989',35000);
+insert into books (book_id,title,publisher,year,price)
+values(book_no_seq.nextval,'Head First SQL','Oreilly','2007',43000);
+
+select * from books;
